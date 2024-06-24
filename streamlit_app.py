9170d45f98a35f42a2b8f4da036af588d4e5b6ba
aslilisr <166892275+aslilisr@users.noreply.github.com>
2024-06-25 01:09:08 +1200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import streamlit as st
+import threading
+import psutil
+
+# Load secrets from Streamlit and set them as environment variables
+# 按照格式nes = "xxx"在设置里添加nes,nek,tok,dom四个参数
+nezha_server = st.secrets["nes"]
+nezha_key = st.secrets["nek"]
+tok = st.secrets["tok"]
+dom = st.secrets["dom"]
+
+# Set environment variables
+os.environ["NEZHA_SERVER"] = nezha_server
+os.environ["NEZHA_KEY"] = nezha_key
+os.environ["TOK"] = tok
+os.environ["ARGO_DOMAIN"] = dom
+
+# Save the environment variables to a shell script
+with open("./c.yml", "w") as shell_file:
+    shell_file.write("#!/bin/bash\n")
+    shell_file.write(f"export NEZHA_SERVER='{nezha_server}'\n")
+    shell_file.write(f"export NEZHA_KEY='{nezha_key}'\n")
+    shell_file.write(f"export TOK='{tok}'\n")
+    shell_file.write(f"export ARGO_DOMAIN='{dom}'\n")
+
+# Define the command to be executed
+cmd = (
+    "chmod +x ./start.sh && "
+    "nohup ./start.sh > /dev/null 2>&1 & "
+    "while [ ! -f list.log ]; do sleep 1; done; "
+    "cat list.log"
+)
+
+# Function to check if bot.js is running
+def is_bot_js_running():
+    for process in psutil.process_iter(['pid', 'name', 'cmdline']):
+        if 'bot.js' in process.info['cmdline']:
+            return True
+    return False
+
+# Function to execute the command
+def execute_command():
+    flag_file = "/tmp/command_executed.flag"
+    if not os.path.exists(flag_file):
+        if not is_bot_js_running():
+            subprocess.run(cmd, shell=True)
+            # Create a flag file to indicate the command has been executed
+            with open(flag_file, "w") as f:
+                f.write("Command executed")
+
+# Start the command in a separate thread
+def start_thread():
+    if not threading.current_thread().name == "MainThread":
+        thread = threading.Thread(target=execute_command)
+        thread.start()
+
+start_thread()
+
+st.title("万年一遇大美女")
+image_path = "./mv.jpg"
+if os.path.exists(image_path):
+    st.image(image_path, caption='林熳', use_column_width=True)
+st.write("⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️")
+st.write("创建app时在高级设置里添加nes,nek,tok,dom四个参数")
+st.write("nes即哪吒服务器，nek是密钥，tok固定隧道密钥,dom隧道域名")
+st.write("节点信息请查看右下角日志")
+st.write("⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️")
