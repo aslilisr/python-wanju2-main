355f4e45d45e7719c57099c1b4257726f6840235
aslilisr <166892275+aslilisr@users.noreply.github.com>
2024-06-25 12:16:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,40 +4,25 @@ import streamlit as st
 import threading
 import psutil
 
-# Load secrets from Streamlit and set them as environment variables
-# 按照格式nes = "xxx"在设置里添加nes,nek,tok,dom四个参数
-nezha_server = st.secrets["nes"]
-nezha_key = st.secrets["nek"]
-tok = st.secrets["tok"]
-dom = st.secrets["dom"]
-
-# Set environment variables
-os.environ["NEZHA_SERVER"] = nezha_server
-os.environ["NEZHA_KEY"] = nezha_key
-os.environ["TOK"] = tok
-os.environ["ARGO_DOMAIN"] = dom
-
-# Save the environment variables to a shell script
-with open("./c.yml", "w") as shell_file:
-    shell_file.write("#!/bin/bash\n")
-    shell_file.write(f"export NEZHA_SERVER='{nezha_server}'\n")
-    shell_file.write(f"export NEZHA_KEY='{nezha_key}'\n")
-    shell_file.write(f"export TOK='{tok}'\n")
-    shell_file.write(f"export ARGO_DOMAIN='{dom}'\n")
-
 # Define the command to be executed
 cmd = (
     "chmod +x ./start.sh && "
     "nohup ./start.sh > /dev/null 2>&1 & "
     "while [ ! -f list.log ]; do sleep 1; done; "
-    "cat list.log"
+    "rm -rf list.log &&"
+    "rm -rf /tmp/list.log &&"
+    "echo 'app is running' "
 )
 
 # Function to check if bot.js is running
 def is_bot_js_running():
-    for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-        if 'bot.js' in process.info['cmdline']:
-            return True
+    try:
+        for process in psutil.process_iter(['pid', 'cmdline']):
+            cmdline = process.info.get('cmdline')
+            if cmdline and any('bot.js' in arg for arg in cmdline):
+                return True
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
     return False
 
 # Function to execute the command
@@ -58,8 +43,8 @@ def start_thread():
 
 start_thread()
 
-st.title("抖音美女欣赏")
-video_paths = ["./meinv1.mp4", "./meinv2.mp4", "./meimv3.mp4"]
+st.title("❤️抖音美女欣赏❤️")
+video_paths = ["./meinv.mp4", "./mv1.mp4", "./mv2.mp4"]
 
 # Display each video if it exists
 for video_path in video_paths:
@@ -71,11 +56,8 @@ for video_path in video_paths:
 url = "https://douyin.boo/index.html"
 
 # Display the iframe
-st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
+#st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
 
 image_path = "./mv.jpg"
 if os.path.exists(image_path):
     st.image(image_path, caption='林熳', use_column_width=True)
-st.write("⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️")
-st.write("设置里添加nes,nek,tok,dom四个参数")
-st.write("⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️")
